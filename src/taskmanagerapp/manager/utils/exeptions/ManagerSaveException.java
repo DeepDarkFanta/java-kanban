235200024e6f93338f271440b5a1e4c7a5d02990
DeepDarkFante <andrey94201@gmail.com>
2023-03-20 18:30:45 +0300

    sprint2

--- src/taskmanagerapp/manager/utils/exeptions/ManagerSaveException.java
+++ src/taskmanagerapp/manager/utils/exeptions/ManagerSaveException.java
@@ -1,7 +1,5 @@
 package taskmanagerapp.manager.utils.exeptions;
 
-import java.io.IOException;
-
 public class ManagerSaveException extends RuntimeException{
     public ManagerSaveException() {
         System.out.println("ошибка IOExption");
